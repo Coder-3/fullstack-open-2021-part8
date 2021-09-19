1f3920a472f05152ea5d07d12069b2f962c096a5
Cedric Quenette <cedricquenette@outlook.com>
2021-09-20 07:27:27 +1000

    8.3: All authors

--- 8.1-8.7/library-backend.js
+++ 8.1-8.7/library-backend.js
@@ -97,10 +97,18 @@ const typeDefs = gql`
     id: ID!
   }
 
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int
+    id: ID!
+  }
+
   type Query {
-    bookCount: Int!
     authorCount: Int!
+    bookCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -108,7 +116,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter(book => book.author === root.name).length
+    }
   }
 }
 
