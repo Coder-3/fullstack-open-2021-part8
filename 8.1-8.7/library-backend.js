2e58ce0b9b72573baedfcb2f157c4c288a928787
Cedric Quenette <cedricquenette@outlook.com>
2021-09-21 06:46:17 +1000

    8.6: Adding a book

--- 8.1-8.7/library-backend.js
+++ 8.1-8.7/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server')
 const { argsToArgsConfig } = require('graphql/type/definition')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -111,6 +112,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -135,6 +145,20 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter(book => book.author === root.name).length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      if (!authors.find(author => author.name === args.author)) {
+        const author = {
+          name: args.author,
+          id: uuid()
+        }
+        authors = authors.concat(author)
+      }
+      return book
     }
   }
 }
